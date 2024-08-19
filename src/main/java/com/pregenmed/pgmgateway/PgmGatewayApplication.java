2677fcb3b020229bc0ac9010f30a631b9ca9d56c
ochab.mat@gmail.com <ochab.mat@gmail.com>
2024-08-19 18:09:59 +0200

    initial commit

---
+++ src/main/java/com/pregenmed/pgmgateway/PgmGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pregenmed.pgmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgmGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pregenmed/pgmgateway/PgmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pregenmed.pgmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
